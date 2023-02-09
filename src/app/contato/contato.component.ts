ed6cc69ab1111a9896a1dafa037485113409560a
leticiaa-s <leticiasqr97@gmail.com>
2023-02-09 18:33:46 -0300

    Validação dos campos do formulário de contato

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
@@ -8,9 +8,41 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+  formContato = this.fb.group({
+    nome: ["",[
+      Validators.minLength(4),
+      Validators.required
+    ]],
+    assunto: ["",[
+      Validators.minLength(10),
+      Validators.required
+    ]],
+    telefone: ["",[
+      Validators.minLength(11),
+      Validators.required
+    ]],
+    email: ["",[
+      Validators.email,
+      Validators.required
+    ]],
+    mensagem: ["",[
+      Validators.minLength(20),
+      Validators.required
+    ]]
+  });
+
+  constructor(
+    private fb: FormBuilder
+    // agrupa todos os campos presentes no formulário para validar seu preenchimento
+  ) {}
 
   ngOnInit(): void {
+  }
+
+  enviarFormulario(){
+    alert("Mensagem enviada com sucesso! Aguarde nosso contato.");
+    this.formContato.reset();
+    location.reload();
   }
 
 }
--- src/app/contato/contato.module.ts
+++ src/app/contato/contato.module.ts
@@ -1,8 +1,12 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContatoRoutingModule } from './contato-routing.module';
 import { ContatoComponent } from './contato.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {IMaskDirective, IMaskDirectiveModule} from 'angular-imask';
 
 @NgModule({
   declarations: [
@@ -11,6 +15,11 @@ import { ContatoComponent } from './contato.component';
   imports: [
     CommonModule,
     ContatoRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    IMaskDirectiveModule
     ]
 })
 export class ContatoModule { }
