e3593bfe964c3ec7e6d20b90aa924cf869547404
leticiaa-s <leticiasqr97@gmail.com>
2023-02-08 21:38:00 -0300

    Criação do formulário de contato

---
+++ src/app/contato/contato.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contato',
+  templateUrl: './contato.component.html',
+  styleUrls: ['./contato.component.css']
+})
+export class ContatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
